8bc58ad66337d4ea6e31f78d6afb02449afeaafc
Amel Plojovic <amelplojovic67@gmail.com>
2024-08-30 23:00:32 -0700

    adding packages

---
+++ project/src/components/coins/Coins.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Coins.css"
+
+const Coins = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Coins
---
+++ project/src/components/exchanges/Exchanges.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "/Exchanges.css"
+
+const Exchanges = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Exchanges
