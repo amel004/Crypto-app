ba98a4c32917afe10709b72341b6ddf9d72c099a
Amel Plojovic <amelplojovic67@gmail.com>
2024-09-03 01:40:45 -0700

    Komitovanje lokalnih promena u App.jsx

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Coins from './components/coins/Coins'
 
 
 const App = () => {
   return (
     <div>
+      <>
       <Navbar />
+      <Coins />
+      </>
       
     </div>
   )
