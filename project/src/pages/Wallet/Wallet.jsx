e30f0e2f9f9c9008475ce836ee070807f1437aa5
Amel Plojovic <amelplojovic67@gmail.com>
2024-09-05 18:10:50 +0200

    user change

--- project/src/pages/Wallet/Wallet.jsx
+++ project/src/pages/Wallet/Wallet.jsx
@@ -3,10 +3,28 @@ import './Wallet.css'
 
 const Wallet = () => {
   return (
-    <div>
-      
+    <div className='wallet'>
+      <div className='heading'>
+        <div className='text'>
+        <h1>
+            Buy Bitcoin & <br /> Crypto
+          </h1>
+          <h3>
+            Sign up today and <span>buy 50+</span>
+            <br /> cryptotocurrencies in minutes <br />
+            Get started with as little as
+            <span>$10</span>
+          </h3>
+        </div>
+  
+      </div>
+
+      <div className='wallet-img'>
+        <img src="/wallet-pic.png" className='picture' />
+      </div>      
     </div>
   )
 }
 
 export default Wallet
+
